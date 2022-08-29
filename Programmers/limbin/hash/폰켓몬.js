98eb4fddbbf0e5bf7d1627f06b34057bf6ae0c6c
bin_lim <bin_lim@tmax.co.kr>
2022-08-29 17:52:11 +0900

    폰켓몬 피드백 적용

--- Programmers/limbin/hash/폰켓몬.js
+++ Programmers/limbin/hash/폰켓몬.js
@@ -1,7 +1,6 @@
 function solution(nums) {
     let length = nums.length / 2;
-    let set = new Set(nums);
-    console.log(`set: `, set.size)
-    if (length >= set.size) return set.size;
-    if (length < set.size) return length;
+    let numsSet = new Set(nums);
+    if (length >= numsSet.size) return numsSet.size;
+    return length;
 }
