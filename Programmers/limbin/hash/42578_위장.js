e1fa1585a39abf1bc5a89cf982ab89f62cf433dc
bin_lim <bin_lim@tmax.co.kr>
2022-08-30 14:55:54 +0900

    [Programmers-42578] 위장

---
+++ Programmers/limbin/hash/42578_위장.js
@@ -0,0 +1,27 @@
+/**처음에는 원본 갯수 + 순열 돌려서 할려고 했는데 ... 너무 복잡해보여서 포기  */
+function solution(clothes) {
+    let answer = [];
+    let count = 0;
+    for (let i = 0; i < clothes.length; i++) {
+        answer.push(clothes[i][1])
+    }
+    const k = answer.sort();
+    let arrSet = new Set(k);
+    console.log(k, k.length)
+    console.log(arrSet, arrSet.size)
+    for (let j = 2; j < arrSet.size + 1; j++) {
+    }
+}
+
+function solution(clothes) {
+    let map = {};
+    let answer = 1;
+    for (let i = 0; i < clothes.length; i++) {
+        map[clothes[i][1]] = map[clothes[i][1]] ? map[clothes[i][1]] + 1 : 2;
+    }
+    for (let key in map) {
+        answer *= map[key];
+    }
+
+    return answer - 1;
+}
