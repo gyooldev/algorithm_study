747f703ce38ab744d8364a6dd01639169a8fe51e
bin_lim <bin_lim@tmax.co.kr>
2022-08-31 15:32:22 +0900

    [Programmers-42578] 위장 주석처리

--- Programmers/limbin/hash/42578_위장.js
+++ Programmers/limbin/hash/42578_위장.js
@@ -1,17 +1,17 @@
 /**처음에는 원본 갯수 + 순열 돌려서 할려고 했는데 ... 너무 복잡해보여서 포기  */
-function solution(clothes) {
-    let answer = [];
-    let count = 0;
-    for (let i = 0; i < clothes.length; i++) {
-        answer.push(clothes[i][1])
-    }
-    const k = answer.sort();
-    let arrSet = new Set(k);
-    console.log(k, k.length)
-    console.log(arrSet, arrSet.size)
-    for (let j = 2; j < arrSet.size + 1; j++) {
-    }
-}
+// function solution(clothes) {
+//     let answer = [];
+//     let count = 0;
+//     for (let i = 0; i < clothes.length; i++) {
+//         answer.push(clothes[i][1])
+//     }
+//     const k = answer.sort();
+//     let arrSet = new Set(k);
+//     console.log(k, k.length)
+//     console.log(arrSet, arrSet.size)
+//     for (let j = 2; j < arrSet.size + 1; j++) {
+//     }
+// }
 
 function solution(clothes) {
     let map = {};
