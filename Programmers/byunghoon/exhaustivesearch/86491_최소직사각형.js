73e31c567fdbdc58d0efdd521c6dddf748a7e5fe
byunghoon_kang <byunghoon_kang@tmax.co.kr>
2022-09-05 16:41:24 +0900

    더 깔끔한 답안(타인) 추가

--- Programmers/byunghoon/exhaustivesearch/86491_최소직사각형.js
+++ Programmers/byunghoon/exhaustivesearch/86491_최소직사각형.js
@@ -23,3 +23,15 @@ function solution(sizes) {
 
   return answer;
 }
+
+// Cleaner Code with Array.reduce()
+function solution(sizes) {
+  const [hor, ver] = sizes.reduce(
+    ([h, v], [a, b]) => [
+      Math.max(h, Math.max(a, b)),
+      Math.max(v, Math.min(a, b)),
+    ],
+    [0, 0]
+  );
+  return hor * ver;
+}
