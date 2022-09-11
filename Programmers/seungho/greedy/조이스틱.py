c02fad73975e5c53a35ee00f4821f1d6168b65cb
shoukou-lee <shoukou.lee@gmail.com>
2022-09-11 20:50:27 +0900

    refactor: 조이스틱

--- Programmers/seungho/greedy/조이스틱.py
+++ Programmers/seungho/greedy/조이스틱.py
@@ -2,21 +2,27 @@ def get_diff(char):
     return min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
 
 
-def solution(name):
-    answer = 0
-
-    left_right = len(name) - 1
-
-    up_down = 0
+def get_minimum_up_down(name):
+    minimum = 0
     for i in range(len(name)):
-        up_down += get_diff(name[i])
+        minimum += get_diff(name[i])
+    return minimum
 
+
+def get_minimum_left_right(name):
+    minimum = len(name) - 1  # 순서대로 읽는 경우
+
+    for i in range(len(name)):
         nxt = i + 1
+
         while nxt < len(name) and name[nxt] == 'A':
             nxt += 1
-        left_right = min(left_right, i + i + len(name) - nxt)
-        left_right = min(left_right, (len(name) - nxt) * 2 + i)
+        # A를 만나면 돌아가는 경우
+        minimum = min(minimum, (i * 2) + (len(name) - nxt))
+        # 먼저 뒤로 돌아갔다 시작점으로 오는 경우
+        minimum = min(minimum, ((len(name) - nxt) * 2) + i)
+    return minimum
 
-    answer = up_down + left_right
 
-    return answer
+def solution(name):
+    return get_minimum_up_down(name) + get_minimum_left_right(name)
