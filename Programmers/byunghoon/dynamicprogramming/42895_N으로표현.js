d56a84167d65b89072fdd2d5a3d2b5cca070f648
KBH <fermionmx@gmail.com>
2022-09-18 22:47:28 +0900

    [Programmers_42895] N으로 표현(Unsolved)

---
+++ Programmers/byunghoon/dynamicprogramming/42895_N으로표현.js
@@ -0,0 +1,22 @@
+//Unsolved
+
+function solution(N, number) {
+  const cache = new Array(9).fill(0).map(() => new Set());
+  for (let i = 1; i < 9; i++) {
+    cache[i].add('1'.repeat(i) * N);
+    for (let j = 1; j < i; j++) {
+      for (const arg1 of cache[j]) {
+        for (const arg2 of cache[i - j]) {
+          cache[i].add(arg1 + arg2);
+          cache[i].add(arg1 - arg2);
+          cache[i].add(arg1 * arg2);
+          cache[i].add((arg1 / arg2) >> 0);
+        }
+      }
+    }
+    if (cache[i].has(number)) return i;
+  }
+  return -1;
+}
+
+solution(5, 12);
