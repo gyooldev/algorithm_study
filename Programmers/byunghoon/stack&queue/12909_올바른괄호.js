ecb2fdc3cbe76c355d76a29baf4f256116436b5a
KBH <fermionmx@gmail.com>
2022-10-01 23:13:40 +0900

    [Programmers-42586] 기능개발

--- Programmers/byunghoon/stack&queue/12909_올바른괄호.js
+++ Programmers/byunghoon/stack&queue/12909_올바른괄호.js
@@ -1,35 +1,13 @@
-function solution(progresses, speeds) {
-  var answer = [];
-  let finishedDateArray = [];
-
-  for (let i = 0; i < speeds.length; i += 1) {
-    let tempProgress = speeds[i] + progresses[i];
-    let tempDate = 1;
-    while (tempProgress < 100) {
-      tempProgress += speeds[i];
-      tempDate += 1;
-    }
-    finishedDateArray.push(tempDate);
+function solution(s) {
+  var answer = true;
+  let parenthesesStack = [];
+  for (let i = 0; i < s.length; i += 1) {
+    if (parenthesesStack[parenthesesStack.length - 1] === '(' && s[i] === ')') {
+      parenthesesStack.pop();
+    } else parenthesesStack.push(s[i]);
   }
-
-  let q = finishedDateArray[0];
-  let deployNumArray = [];
-  let deployNum = 1;
-
-  for (let i = 1; i < finishedDateArray.length; i += 1) {
-    if (q < finishedDateArray[i]) {
-      deployNumArray.push(deployNum);
-      deployNum = 1;
-      q = finishedDateArray[i];
-    } else deployNum += 1;
-
-    if (i === finishedDateArray.length - 1) {
-      deployNumArray.push(deployNum);
-    }
-  }
-
-  answer = deployNumArray;
+  answer = !parenthesesStack.length;
   return answer;
 }
 
-console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
+//Programmers 효율성 통과 못하는 문제 있어서 실제로는 s.split('')해서 array 다루는 방식으로 제출
---
+++ Programmers/byunghoon/stack&queue/42586_기능개발.js
@@ -0,0 +1,33 @@
+function solution(progresses, speeds) {
+  var answer = [];
+  let finishedDateArray = [];
+
+  for (let i = 0; i < speeds.length; i += 1) {
+    let tempProgress = speeds[i] + progresses[i];
+    let tempDate = 1;
+    while (tempProgress < 100) {
+      tempProgress += speeds[i];
+      tempDate += 1;
+    }
+    finishedDateArray.push(tempDate);
+  }
+
+  let q = finishedDateArray[0];
+  let deployNumArray = [];
+  let deployNum = 1;
+
+  for (let i = 1; i < finishedDateArray.length; i += 1) {
+    if (q < finishedDateArray[i]) {
+      deployNumArray.push(deployNum);
+      deployNum = 1;
+      q = finishedDateArray[i];
+    } else deployNum += 1;
+
+    if (i === finishedDateArray.length - 1) {
+      deployNumArray.push(deployNum);
+    }
+  }
+
+  answer = deployNumArray;
+  return answer;
+}
